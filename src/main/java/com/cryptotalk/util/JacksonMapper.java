8049d543ad9a1681875467ea60721915ae25d42e
ashrestha <aseem.shrestha@factset.com>
2018-03-03 02:31:33 -0500

    formatting changes

--- src/main/java/com/cryptotalk/util/JacksonMapper.java
+++ src/main/java/com/cryptotalk/util/JacksonMapper.java
@@ -6,24 +6,27 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public enum JacksonMapper {
-	INSTANCE;
+public enum JacksonMapper
+{
+    INSTANCE;
 
-	private final ObjectMapper mapper = new ObjectMapper();
+    private final ObjectMapper mapper = new ObjectMapper();
 
-	private JacksonMapper() {
-		mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-		mapper.disable(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES);
-		mapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
-		mapper.enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT);
-		mapper.enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY);
-		mapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
-		mapper.configure(JsonGenerator.Feature.ESCAPE_NON_ASCII, true);
+    private JacksonMapper()
+    {
+        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        mapper.disable(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES);
+        mapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
+        mapper.enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT);
+        mapper.enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY);
+        mapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
+        mapper.configure(JsonGenerator.Feature.ESCAPE_NON_ASCII, true);
 
-	}
+    }
 
-	public ObjectMapper getObjectMapper() {
-		return mapper;
-	}
+    public ObjectMapper getObjectMapper()
+    {
+        return mapper;
+    }
 
 }
