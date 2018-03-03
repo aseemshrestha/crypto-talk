1ccfe3d6a6d8534e73dde21b22be9ea9c3eed806
ashrestha <aseem.shrestha@factset.com>
2018-03-03 03:18:06 -0500

    add error handling

--- src/main/java/com/cryptotalk/controller/HomeController.java
+++ src/main/java/com/cryptotalk/controller/HomeController.java
@@ -1,14 +1,14 @@
 package com.cryptotalk.controller;
 
 import java.util.Map;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.cryptotalk.service.NewsService;
+import com.cryptotalk.util.ResourceNotFoundException;
 import com.cryyptotalk.generated.News;
 
 @RestController
@@ -23,13 +23,19 @@ public class HomeController
     }
 
     @RequestMapping( value = "/", method = RequestMethod.GET, produces = { "application/json" } )
-    public @ResponseBody Map<String, News> Home(@RequestParam( value = "cache", required = false ) String cache)
+    public ResponseEntity<?> Home(@RequestParam( value = "cache", required = false ) String cache)
     {
         if ("1".equals(cache)) {
             this.newsService.getNewsMap().clear();
         }
+
         Map<String, News> news = this.newsService.getNews();
-        return news;
+
+        if (news == null) {
+            throw new ResourceNotFoundException("News Resource Not Found");
+        }
+
+        return new ResponseEntity<>(news, HttpStatus.OK);
 
     }
 
---
+++ src/main/java/com/cryptotalk/util/RestExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.cryptotalk.util;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler
+{
+    @ExceptionHandler( ResourceNotFoundException.class )
+    public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException rnfe, HttpServletRequest request)
+    {
+        ErrorDetail errorDetail = new ErrorDetail();
+        errorDetail.setTitle("Resource Not Found");
+        errorDetail.setStatus(HttpStatus.NOT_FOUND.value());
+        errorDetail.setDetail(rnfe.getMessage());
+        errorDetail.setDeveloperMessage(rnfe.getClass().getName());
+
+        return new ResponseEntity<>(errorDetail, null, HttpStatus.NOT_FOUND);
+
+    }
+}
